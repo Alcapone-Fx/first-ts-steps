a4a52a507cdc13543c873b4eaf3a35f2a992b018
Felix Navas <navasfx19@gmail.com>
2020-10-29 12:38:46 -0600

    :sparkles: Classes, inheritance, getter, setter & static keyword
    Problem
    TS approach of Classes, inheritance, getter, setter & static keyword
    
    Solution
    app.ts contains a simple example of all this features

---
+++ src/app.ts
@@ -0,0 +1,118 @@
+/**
+ * Static methods and properties are accesible via Class
+ * and not via instance.
+ *
+ * To make a property or method static simply
+ * add "static" keyword to the beginning
+ *
+ *
+ */
+
+class Department {
+  // private readonly id: string;
+  // private name: string;
+  protected employees: string[] = [];
+  static fiscalYear = 2020;
+
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}
+    Fiscal year: ${Department.fiscalYear}`);
+  }
+
+  addEmployee(employee: string) {
+    // validation etc
+    // this.id = 'd2';
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
+
+class AccountingDepartment extends Department {
+  private lastReport: string;
+  /**
+   * Getter
+   */
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found.");
+  }
+
+  /**
+   * Setter
+   */
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value!");
+    }
+    this.addReport(value);
+  }
+
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+
+  addEmployee(name: string) {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment("d1", ["Max"]);
+
+it.addEmployee("Max");
+it.addEmployee("Manu");
+
+// it.employees[2] = 'Anna';
+
+it.describe();
+it.name = "NEW NAME";
+it.printEmployeeInformation();
+
+console.log(it);
+
+const accounting = new AccountingDepartment("d2", []);
+
+accounting.mostRecentReport = "Year End Report";
+accounting.addReport("Something went wrong...");
+console.log(accounting.mostRecentReport);
+
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
+accounting.printReports();
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+
+// accountingCopy.describe();
