c94185cae42b6c62f0eea2c1b13f094678e6ffba
Felix Navas <navasfx19@gmail.com>
2020-10-29 17:44:26 -0600

    :sparkles: Interfaces as function types Problem Know about interfaces as function types
    Solution
    Simple example of how to use interfaces as function types

--- src/app.ts
+++ src/app.ts
@@ -3,26 +3,38 @@
  * and we can use it as a type check that objects must have
  * the interface structure
  */
-interface Named{
+
+/**
+ * Interface can be used as function types
+ */
+interface addFunctionType {
+  (a: number, b: number): void;
+}
+
+let add: addFunctionType;
+
+add = (a, b) => a + b;
+
+interface Named {
   readonly name: string;
 }
 
 /**
- * Different to classes, interfaces can extend more than one 
+ * Different to classes, interfaces can extend more than one
  * interface
  */
-interface Greetable extends Named{
+interface Greetable extends Named {
   greet(phrase: string): void;
 }
 
-class Person implements Greetable{
+class Person implements Greetable {
   age: number;
-  
-  constructor(public name: string){
+
+  constructor(public name: string) {
     this.age = 30;
   }
-  
-  greet(phrase: string){
+
+  greet(phrase: string) {
     console.log(`${phrase} ${this.name}`);
   }
 }
@@ -30,4 +42,3 @@ class Person implements Greetable{
 let user1 = new Person("Felix");
 
 user1.greet("Hi there - I am");
- 
