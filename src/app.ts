be17565b2f17bc0ef1020c631705a3073174264a
Felix Navas <navasfx19@gmail.com>
2020-10-30 11:42:32 -0600

    :sparkles: Discriminated Unions Problem Discriminated Unions
    Solution
    Simple example of the topic

--- src/app.ts
+++ src/app.ts
@@ -27,9 +27,111 @@ const e1: ElevatedEmployee = {
 };
 
 //Union type
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(employee: UnknownEmployee) {
+  console.log(`Name: ${employee.name}`);
+  /*
+   * Type guard
+   */
+  if ("privileges" in employee) {
+    console.log(`Privileges: ${employee.privileges}`);
+  }
+  if ("startDate" in employee) {
+    console.log(`Start Date: ${employee.startDate}`);
+  }
+}
+
+printEmployeeInformation(e1);
+printEmployeeInformation({ name: "Kratos", startDate: new Date() });
+
+/*
+ * Our type guard on classes is the instace of
+ */
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving...");
+  }
+
+  loadCargo(amount: number) {
+    console.log(`Loading cargo...${amount}`);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const vehicle1 = new Car();
+const vehicle2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  //type guard
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(100);
+  }
+}
+
+useVehicle(vehicle1);
+useVehicle(vehicle2);
+
+//Union type
 type Combinable = string | number;
 //Union type
 type Numeric = number | boolean;
 
-//: number is the result of the Intersection of Combinable & Numeric 
+//: number is the result of the Intersection of Combinable & Numeric
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  /*
+   * Type Guards help us with union types
+   * Help us to use the flexibility of union types and
+   * ensure that our code runs correctly at runtime
+   */
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  } else {
+    return a + b;
+  }
+}
+
+/*
+ * Discriminated Unions it's a pattern that help us to know
+ * and to be confident what type of object we want to work with
+ * The interfaces has a the same property but different type
+ */
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+  }
+
+  console.log(`Moving at speed: ${speed}`);
+}
+
+moveAnimal({ type: "horse", runningSpeed: 10 });
+moveAnimal({ type: "bird", flyingSpeed: 15 });
