fe24f01c4aa180cb8e6f05dbf22b154c48534b1c
Felix Navas <navasfx19@gmail.com>
2020-11-10 15:28:04 -0600

    :sparkles: Generic functions and interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 /**
  * A Generic Type is a type wich is kind of connected with some other type
  * and is really flexible regarding wich axact type what other type is
- * Generic types help us to have more information about the type to store or the 
+ * Generic types help us to have more information about the type to store or the
  * connected type
  */
 
@@ -9,7 +9,7 @@
  * The simplest example of generic types is the array.
  * The array by itself is a type "a list of data"
  * An array stores certain kind of data in it
- * 
+ *
  * In that notation we specify the type of data should go into the array
  * const names: Array<string> = [] === const names: string[] = []
  */
@@ -21,11 +21,10 @@
 
 // promise.then((data) => {data.length});
 
-
 /**
  * In this case we are telling to TS the function will recieve two types,
  * maybe different types and TS understand that
- * 
+ *
  * Type Constraints
  * With type constriants we are telling to TS that objectA and B could be whatever,
  * but mut be an object
@@ -35,5 +34,28 @@ function merge<T extends object, U extends object>(objectA: T, objectB: U) {
   return Object.assign(objectA, objectB);
 }
 
-const mergedObject = merge({name: 'Felix', hobbies:['Coding', 'Sex', 'Video games']}, {age: 29});
+const mergedObject = merge(
+  { name: 'Felix', hobbies: ['Coding', 'Sex', 'Video games'] },
+  { age: 29 }
+);
 console.log(mergedObject);
+
+/*
+ * Another Generic Function
+ * We care only for the object that the function recieves contains a lenght property, nothing else
+ */
+interface ILengthy {
+  length: number;
+}
+
+function countAndPrint<T extends ILengthy>(element: T) {
+  let descriptionText = 'Got no value';
+
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element';
+  } else if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements`;
+  }
+
+  return [element, descriptionText];
+}
