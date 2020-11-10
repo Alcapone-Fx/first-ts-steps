f17e686a5fb87974bf15c270e77cfdbe71d85596
Felix Navas <navasfx19@gmail.com>
2020-11-10 14:31:23 -0600

    :sparkles: Generic function, creating a generic function

--- src/app.ts
+++ src/app.ts
@@ -13,10 +13,25 @@
  * In that notation we specify the type of data should go into the array
  * const names: Array<string> = [] === const names: string[] = []
  */
-const names: Array<string> = [];
+// const names: Array<string> = [];
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {resolve('This has done'); reject('Error')}, 1000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {resolve('This has done'); reject('Error')}, 1000);
+// });
 
-promise.then((data) => {data.length});
+// promise.then((data) => {data.length});
+
+
+/**
+ * In this case we are telling to TS the function will recieve two types,
+ * maybe different types and TS understand that
+ * @param objectA 
+ * @param objectB 
+ */
+
+function merge<T, U>(objectA: T, objectB: U) {
+  return Object.assign(objectA, objectB);
+}
+
+const mergedObject = merge({name: 'Felix', hobbies:['Coding', 'Sex', 'Video games']}, {age: 29});
+console.log(mergedObject);
