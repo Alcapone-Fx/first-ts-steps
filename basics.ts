e6dc436f8443ec8c8e0709f3562e8edbb4c10efa
Felix Navas <navasfx19@gmail.com>
2022-12-22 22:08:21 -0600

    ✨ Basic types and type inference

--- basics.ts
+++ basics.ts
@@ -12,8 +12,12 @@
  * @param n1 First param to add
  * @param n2 Second param to add
  */
-function add(n1: number, n2:number) {
-    return `El resultado es: ${n1 + n2}`;
+function add(n1: number, n2:number, showResult: boolean, phrase: string) {
+    const result = n1 + n2;
+    if (!showResult) {
+        return `El resultado es: ${result}`;
+    }
+    console.log(`${phrase} ${result}`)
 }
 
 /**
@@ -22,9 +26,12 @@ function add(n1: number, n2:number) {
 const number1 = 2;
 const number2 = 4;
 let number3: number;
+const printResult = true;
+const resultPhrase = 'Result is:';
 
 number3 = 0x2;
 
-let result = add(number1, number2);
+// let result = add(number1, number2, printResult);
+add(number1, number2, printResult, resultPhrase);
 
-console.log(result);
+// console.log(result);
