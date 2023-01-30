84042e7c42f926f18422f91d15685fc582a1dfa2
Felix Navas <navasfx19@gmail.com>
2023-01-30 15:08:20 -0600

    ✨ Accessor, Method and Param decorator

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,9 @@
  * We can define a decorator factory, which returns a decorator function,
  * but allow us to configure it when we assign it as a decorator to something
  */
+/**
+ * Decorators are functions that are executed when the class is defined to add extra functionality
+ */
 const Logger = (log: string) => (constructor: Function) => {
   console.log(log);
   console.log(constructor);
@@ -43,11 +46,42 @@ const Log = (target: any, propName: string | Symbol) => {
   console.log(target, propName);
 };
 
+/**
+ * Accessor decorator
+ */
+const AccesorParam = (target: any, nameMember: string, propDescriptor: PropertyDescriptor) => {
+  console.log('Accessor decorator');
+  console.log('target', target);
+  console.log('nameMember', nameMember);
+  console.log('propDescriptor', propDescriptor);
+};
+
+/**
+ *  Method decorator
+ */
+const MethodDecorator = (target: any, nameMethod: string | Symbol, propDescriptor: PropertyDescriptor) => {
+  console.log('Method decorator');
+  console.log('target', target);
+  console.log('nameMethod', nameMethod);
+  console.log('propDescriptor', propDescriptor);
+}
+
+/**
+ *  Parameter decorator
+ */
+const ParamDecorator = (target: any, name: string | Symbol, position: number) => {
+  console.log('Method decorator');
+  console.log('target', target);
+  console.log('name', name);
+  console.log('position', position);
+}
+
 class Product {
   @Log
   title: string;
   private _price: number;
 
+  @AccesorParam
   set price (value: number) {
     if(value > 0) {
       this.price = value;
@@ -61,7 +95,8 @@ class Product {
     this._price = price;
   }
 
-  getPriceWithTax (tax: number) {
+  @MethodDecorator
+  getPriceWithTax (@ParamDecorator tax: number) {
     return this._price * (1 + tax);
   }
 }
